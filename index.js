584a3c7a8039659d0a4a3b44837472408ae49db2
OXR <RJ7xx@icloud.com>
2026-01-30 18:08:37 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+process.removeAllListeners('warning');
+
+const CONFIG = {
+    TWITTER_API_KEY: '',
+    OPENROUTER_API_KEY: '',
+    REFRESH_INTERVAL: 2000,
+
+    X_USERNAME: '',
+    X_EMAIL: '',
+    X_PASSWORD: '',
+    X_TOTP: '',
+
+    PROXY: '',
+};
+
+async function main() {
+
+}
+
+main().catch(console.error);
